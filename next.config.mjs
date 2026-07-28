c735ed5a9b60d812bf636aee48cf911532eb0adf
mqasimbilal13 <mqasimbilal13@gmail.com>
2026-07-28 22:57:08 +0500

    Permanent Redirection Added

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source:
+          "/blog/best-typing-exercises-and-drills-to-improve-speed-and-accuracy",
+        destination:
+          "/blog/best-typing-exercise-to-improve-speed-and-accuracy",
+        permanent: true,
+      },
+      {
+        source:
+          "/blog/typing-test-challenge-how-your-wpm-compares-to-the-average-in-2025",
+        destination:
+          "/blog/online-typing-tool-for-free-the-monkey-type",
+        permanent: true,
+      },
+      {
+        source:
+          "/blog/how-to-improve-typing-speed-10-expert-tips-that-actually-work",
+        destination:
+          "/blog/how-to-increase-typing-speed-from-30-wpm-to-60-wpm-complete-beginner-s-guide",
+        permanent: true,
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "img.clerk.com" },
